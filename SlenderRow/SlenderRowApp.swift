bbc3ae6e4c40c879e6eb998af781cfdfca147b2b
lord-of-the-lost <ignatov.99@yandex.ru>
2023-10-16 16:03:08 +0300

    Initial Commit

---
+++ SlenderRow/SlenderRowApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlenderRowApp.swift
+//  SlenderRow
+//
+//  Created by Николай Игнатов on 16.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct SlenderRowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
