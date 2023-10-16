cf98757a9df3153de0a005bb841374231b86b189
lord-of-the-lost <ignatov.99@yandex.ru>
2023-10-16 18:33:52 +0300

    add slender row

--- SlenderRow/ContentView.swift
+++ SlenderRow/ContentView.swift
@@ -8,17 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var changeLayout = true
+    let squaresCount: Int
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        let layout = changeLayout ? AnyLayout(HStackLayout(spacing: 10)) : AnyLayout(VStackLayout(spacing: 0))
+
+        GeometryReader(content: { proxy in
+            layout {
+                let verticalSquareSize = proxy.size.height / CGFloat(squaresCount)
+
+                let squaresArray = changeLayout ? Array(0..<squaresCount) : Array(0..<squaresCount).reversed()
+
+                ForEach(squaresArray, id: \.self) { index in
+                    Button {
+                        withAnimation {
+                            changeLayout.toggle()
+                        }
+                    } label: {
+                        Rectangle()
+                            .background(.blue)
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                            .aspectRatio(1.0, contentMode: .fit)
+                    }
+                    .offset(
+                        x: changeLayout ? 0.0 : CGFloat(index) * (proxy.size.width - verticalSquareSize) / CGFloat(squaresCount - 1),
+                        y: changeLayout ? proxy.size.height / 2.0 : 0.0
+                    )
+                }
+            }
+        })
     }
 }
 
 #Preview {
-    ContentView()
+    ContentView(squaresCount: 7)
 }
--- SlenderRow/SlenderRowApp.swift
+++ SlenderRow/SlenderRowApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SlenderRowApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(squaresCount: 7)
         }
     }
 }
